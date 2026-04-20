c6a18fc3c87453c93824a3e74715ef4c94d2683f
N0ctane._.Dev <n0ctaneDev@gmail.com>
2026-04-20 10:19:20 +0530

    Update page.tsx

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -10,7 +10,7 @@ export default function HomePage() {
         {/* <Hero /> */}
         <div className="flex flex-col z-2 px-4 size-full md:p-12 max-md:items-center max-md:text-center">
           <h1 className="text-4xl my-8 leading-tighter font-medium xl:text-5xl xl:mb-12">
-            A fumadocs template
+            Testing
           </h1>
           <div className="flex flex-row items-center justify-center gap-4 flex-wrap w-full">
             <Link href="/docs/en/components" className={cn(buttonVariants(), 'max-sm:text-sm')}>
